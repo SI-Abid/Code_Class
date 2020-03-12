bc23424a8b14aa65560a89dec9a8d9f49dee414b
SI Abid <siaabid5323@gmail.com>
2020-03-12 08:04:55 +0600

    cfc div2 1B

---
+++ spreadsheets.cpp
@@ -0,0 +1,30 @@
+#include "bits/stdc++.h"
+#define MP make_pair
+#define PB push_back
+#define PI 3.1415926535897932384626433832795
+#define MOD 1000000007
+using namespace std;
+using ll = long long;
+using uint = unsigned int;
+using pii = pair<int, int>;
+using vi = vector <int>;
+
+int main()
+{
+	int n;
+	cin>>n;
+	while(n--)
+	{
+		string s;
+		cin>>s;
+		if(isalnum(s[1]))
+		{
+			
+		}
+		else
+		{
+			
+		}
+	}
+	
+}
