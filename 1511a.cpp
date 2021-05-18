7459e701129a2da896281f69cfd1be87ff5031e7
SI-Abid <siaaid5323@gmai.com>
2021-05-18 17:41:43 +0600

    cfc

---
+++ 1511a.cpp
@@ -0,0 +1,48 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define be begin()
+#define en end()
+#define all(x) (x).begin(),(x).end()
+#define out cout<<setprecision(20)
+
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef set<int> si;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+
+int main(int argc, char* argv[])
+{
+    if(argc == 2 or argc == 3) freopen(argv[1], "r", stdin);
+    if(argc == 3) freopen(argv[2], "w", stdout);
+    ios::sync_with_stdio(false);
+
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, cnt=0, x;
+        cin>>n;
+        while(n--)
+        {
+            cin>>x;
+            if(x&1)
+            {
+                cnt++;
+            }
+        }
+        cout<<cnt<<endl;
+    }
+    
+    return 0;
+}
---
+++ 1511b.cpp
@@ -0,0 +1,49 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define be begin()
+#define en end()
+#define all(x) (x).begin(),(x).end()
+#define out cout<<setprecision(20)
+
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef set<int> si;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+
+int main(int argc, char* argv[])
+{
+    if(argc == 2 or argc == 3) freopen(argv[1], "r", stdin);
+    if(argc == 3) freopen(argv[2], "w", stdout);
+    ios::sync_with_stdio(false);
+
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a, b, c;
+        cin>>a>>b>>c;
+        cout<<"1";
+        a-=c, b--, c--;
+        while(a-->0)
+            cout<<"1";
+        while(c-->0)
+            cout<<"0";
+        cout<<" 1";
+        while(b-->0)
+            cout<<"0";
+        cout<<endl;
+    }
+    
+    return 0;
+}
